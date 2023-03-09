97c702e14fe955d7f853cf45708eab1c63f0758f
ChrisNieves <nievescs20@gmail.com>
2023-03-09 02:52:53 -0500

    removed remenants of react dnd from TaskContainer

--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -1,11 +1,8 @@
 //@ts-nocheck
 import React from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { TaskInterface } from "../models/taskInterface";
 import { useTasks } from "../context/TaskProvider";
 import TaskColumn from "./TaskColumn";
-import { HTML } from "react-dnd-html5-backend/dist/NativeTypes";
 
 type Props = {};
 
@@ -24,30 +21,28 @@ const TaskContainer = (props: Props) => {
   );
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="flex h-[80vh] items-center justify-center">
-        <div className="flex w-[95vw] justify-between">
-          <TaskColumn
-            name="Un-Started Tasks"
-            tasksArray={unstartedTasks}
-            setTasksArray={setTasks}
-            columnNumber={1}
-          />
-          <TaskColumn
-            name="In-Progress Tasks"
-            tasksArray={inprogressTasks}
-            setTasksArray={setTasks}
-            columnNumber={2}
-          />
-          <TaskColumn
-            name="Completed Tasks"
-            tasksArray={completedTasks}
-            setTasksArray={setTasks}
-            columnNumber={3}
-          />
-        </div>
+    <div className="flex h-[80vh] items-center justify-center">
+      <div className="flex w-[95vw] justify-between">
+        <TaskColumn
+          name="Un-Started Tasks"
+          tasksArray={unstartedTasks}
+          setTasksArray={setTasks}
+          columnNumber={1}
+        />
+        <TaskColumn
+          name="In-Progress Tasks"
+          tasksArray={inprogressTasks}
+          setTasksArray={setTasks}
+          columnNumber={2}
+        />
+        <TaskColumn
+          name="Completed Tasks"
+          tasksArray={completedTasks}
+          setTasksArray={setTasks}
+          columnNumber={3}
+        />
       </div>
-    </DndProvider>
+    </div>
   );
 };
 
