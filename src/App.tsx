1628746185bbff0ceba2f2042e19926e0104a387
ChrisNieves <nievescs20@gmail.com>
2023-03-09 05:13:27 -0500

    more work to stop mobile use

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Main from "./components/Main";
 import { TaskProvider } from "./context/TaskProvider";
 import useLocalStorage from "./components/hooks/useLocalStorage";
@@ -7,14 +8,27 @@ const isMobile =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   );
-console.log(isMobile);
+
+if (isMobile) {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+  });
+}
 
 function App() {
   const [task, setTask] = useLocalStorage("tasks", []);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  window.addEventListener("resize", () => displayWindowSize());
+
+  function displayWindowSize() {
+    setWindowWidth(window.innerWidth);
+    console.log("after", windowWidth);
+  }
 
   return (
     <div className="App">
-      <TaskProvider>{isMobile ? <Sorry /> : <Main />}</TaskProvider>
+      <TaskProvider>{windowWidth < 800 ? <Sorry /> : <Main />}</TaskProvider>
     </div>
   );
 }
